116f14607497cd200274139db2a0be0a32a12972
shinjiyun <ranji12@naver.com>
2024-05-29 13:01:04 +0900

    모듈 파일 안의 내용들을 모두 app.js에 직접 작성

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const add = require("./module.js");
-const animal = require("./secondTest.js");
+function add(a,b){
+  return a+b;
+}
+const animal ={
+  zoo : ["사자", "호랑이", "코끼리"],
+  home : ["강아지","고양이","고슴도치"]
+};
 console.log(add(1,2));
 
 let array =[];
